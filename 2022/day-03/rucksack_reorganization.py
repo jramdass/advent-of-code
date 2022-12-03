f5e2bbb698f3d271577965a7b02d2ac7beac5448
Jermaine <jramdass@ryerson.ca>
2022-12-03 01:20:21 -0500

    Day 03 solution

---
+++ 2022/day-03/rucksack_reorganization.py
@@ -0,0 +1,59 @@
+def read_input():
+    input_values = []
+
+    file = open("input")
+    for line in file:
+        input_values.append(line.strip('\n'))
+
+    return input_values
+
+
+ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+if __name__ == "__main__":
+    values = read_input()
+
+    # part 1
+    comp1 = []
+    comp2 = []
+    for row in values:
+        comp1.append(row[0:int(len(row)/2)])
+        comp2.append(row[int(len(row)/2):])
+
+    match = []
+    for c,v in enumerate(comp1):
+        for letter in v:
+            if comp2[c].find(letter) >= 0:
+                match.append(letter)
+                break
+
+    total = 0
+    for item in match:
+        total += ALPHABET.rfind(item)+1
+
+    print(total)
+
+
+    # part 2
+    comp = []
+    index = 0
+    for c,v in enumerate(values):
+        if c == index+2:
+            comp.append((values[c-2], values[c-1], values[c]))
+            index += 3
+
+    match = []
+    for words in comp:
+        word1 = words[0]
+        word2 = words[1]
+        word3 = words[2]
+        for letter in word1:
+            if word2.find(letter) >= 0 and word3.find(letter) >= 0:
+                match.append(letter)
+                break
+
+    total = 0
+    for item in match:
+        total += ALPHABET.rfind(item)+1
+
+    print(total)
