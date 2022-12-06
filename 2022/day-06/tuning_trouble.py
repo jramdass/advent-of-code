b055547b03b91720f840306027e4b790008cf035
Jermaine <jramdass@ryerson.ca>
2022-12-06 00:23:16 -0500

    Day 06 solution

---
+++ 2022/day-06/tuning_trouble.py
@@ -0,0 +1,42 @@
+def read_input():
+    input_values = []
+
+    file = open("input")
+    for line in file:
+        input_values.append(line.strip('\n'))
+
+    return input_values
+
+
+if __name__ == "__main__":
+    values = read_input()
+
+    # part 1
+    distincts = 4
+
+    values = values[0]
+    window = []
+    for c,v in enumerate(values):
+        if c-distincts-1 > 0:
+            window = []
+            for i in range(distincts):
+                window.append(values[c-i])
+
+            if(len(set(window))) >= distincts:
+                print(c+1)
+                break
+
+    # part 2
+    distincts = 14
+
+    window = []
+    for c,v in enumerate(values):
+        if c-distincts-1 > 0:
+            window = []
+            for i in range(distincts):
+
+                window.append(values[c-i])
+
+            if(len(set(window))) >= distincts:
+                print(c+1)
+                break
